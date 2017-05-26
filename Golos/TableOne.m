768cf864ef9b2a7ee32d457d2d23058714386a64
incmisanthrope <incmisanthrope@gmail.com>
2017-05-26 13:50:44 +0300

    After rename

--- Golos/TableOne.m
+++ Golos/TableOne.m
@@ -11,38 +11,24 @@
 
 @interface TableOne ()
 
-@property (strong , nonatomic) NSMutableArray *arrCell, *arrPlist;
+@property (strong , nonatomic) NSArray *arraTbleView;
 @property (strong, nonatomic) NSArray *array;
+@property (assign, nonatomic) NSInteger *vartabone;
 
 @end
 
 @implementation TableOne
-NSInteger *vartabone;
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:YES];
-    self.arrCell = [[NSMutableArray alloc] initWithObjects:@"categor 1", @"categor 2", @"categor 3",@"categor 4",@"categor 5", nil];
-//    NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"]];
-//    NSLog(@"dictionary = %@", dictionary);
-//    
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"plist"];
-//    NSArray *array =  [dictionary objectForKey:@"qu"];
-//    
-//    NSMutableArray *arrPlist = [[NSMutableArray alloc] initWithContentsOfFile:path];
-//    
-//    NSLog(@"array = %@", array);
-//    NSLog(@"array = %@", arrPlist);
+    self.arraTbleView = @[@"categor 1", @"categor 2", @"categor 3",@"categor 4",@"categor 5"];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,30 +38,18 @@ NSInteger *vartabone;
 
 #pragma mark - Table view data source
 
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Incomplete implementation, return the number of sections
-//    return 0;
-//}
-//
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return  self.arrCell.count;
+    return  self.arraTbleView.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
         
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell1" forIndexPath:indexPath];
-    
-//    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//    NSString *documentsDirectory = [paths objectAtIndex:0];
-//    
-//    NSString *recentFilePath = [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
-//    NSArray *history = [NSArray arrayWithContentsOfFile:recentFilePath];
-    
-    
 
-    NSString *aaaaaaaStr = [_arrCell objectAtIndex:indexPath.row];
-    cell.textLabel.text = aaaaaaaStr;
+    NSString *stingCell = [_arraTbleView objectAtIndex:indexPath.row];
+    cell.textLabel.text = stingCell;
     
     return cell;
 }
@@ -88,20 +62,18 @@ NSInteger *vartabone;
         switch (indexPath.row) {
         case 0:
             NSLog(@"case 0 selected");
-//                [tabTwo choiseVarOne:indexPath.item];
-                vartabone = indexPath.row;
-                NSLog(@"value swotch return, %ld",(long) vartabone);
+                _vartabone = indexPath.row;
+                NSLog(@"value swotch return, %ld",(long) _vartabone);
             break;
             case 1:
             NSLog(@"case 1 selected");
-//                [tabTwo choiseVarOne:indexPath.item];
-                vartabone = indexPath.row;
-                NSLog(@"value swotch return, %ld",(long) vartabone);
+                _vartabone = indexPath.row;
+                NSLog(@"value swotch return, %ld",(long) _vartabone);
             break;
             case 2:
             NSLog(@"case 2 selected");
-                vartabone = indexPath.row;
-                NSLog(@"value swotch return, %ld",(long) vartabone);
+                _vartabone = indexPath.row;
+                NSLog(@"value swotch return, %ld",(long) _vartabone);
                 break;
         default:
             break;
@@ -110,52 +82,8 @@ NSInteger *vartabone;
 }
 
 -(NSInteger) returnUserChoise{
-    return vartabone;
+    return *(_vartabone);
 }
 
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Golos/TableTwo.m
+++ Golos/TableTwo.m
@@ -12,8 +12,8 @@
 //
 
 @interface TableTwo ()
-@property (strong, nonatomic) NSMutableArray *categorOne, *categorTwo, *categorTr, *categorFou, *categorFiv;
-@property (strong, nonatomic) NSString *aStr;
+@property (strong, nonatomic) NSArray *arrayCategorOne, *arrayCategorTwo, *arrayCategorThree, *arrayCategorFour, *arrayCategorFive;
+@property (strong, nonatomic) NSString *strTableViewCell;
 @property NSInteger *var;
 @end
 
@@ -24,126 +24,56 @@ NSInteger *var;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:YES];
-    self.categorOne = [[NSMutableArray alloc] initWithObjects:@"catOne_1", @"catOne_2", @"catOne_3",@"catOne_4",@"catOne_5", nil];
-    self.categorTwo = [[NSMutableArray alloc] initWithObjects:@"catTwo_1", @"catTwo_2", @"catTwo_3",@"catTwo_4",@"catTwo_5", nil];
-    self.categorTr  = [[NSMutableArray alloc] initWithObjects:@"1", @"2", @"3",@"4",@"5", nil];
-    self.categorFou = [[NSMutableArray alloc] initWithObjects:@"1", @"2", @"3",@"4",@"5", nil];
-    self.categorFiv = [[NSMutableArray alloc] initWithObjects:@"1", @"2", @"3",@"4",@"5", nil];
+    self.arrayCategorOne = @[@"catOne_1", @"catOne_2", @"catOne_3",@"catOne_4",@"catOne_5"];
+    self.arrayCategorTwo = @[@"catTwo_1", @"catTwo_2", @"catTwo_3",@"catTwo_4",@"catTwo_5"];
+    self.arrayCategorThree  = @[@"1", @"2", @"3",@"4",@"5"];
+    self.arrayCategorFour = @[@"1", @"2", @"3",@"4",@"5"];
+    self.arrayCategorFive = @[@"1", @"2", @"3",@"4",@"5"];
 
 
     TableOne *tabOne = [TableOne new];
 
     var = [tabOne returnUserChoise];
     NSLog(@"%ld",(long) var);
-    
-//    switch ((long) var) {
-//        case 0:
-//            NSLog(@"switch 0 tabTwo");
-//            break;
-//            
-//        default:
-//            break;
-//    }
-    
+
     
 }
 
 #pragma mark - Table view data source
 
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-////#warning Incomplete implementation, return the number of sections
-//    return 0;
-//}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//#warning Incomplete implementation, return the number of rows
-    return self.categorOne.count;
+    return self.arrayCategorOne.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell2" forIndexPath:indexPath];
     
-//    NSString *aStr;
     
     switch ((long) var) {
         case 0:
             NSLog(@"table view TWO switch work");
-            _aStr = [_categorOne objectAtIndex:indexPath.row];
+            _strTableViewCell = [_arrayCategorOne objectAtIndex:indexPath.row];
             break;
         
         case 1:
-            _aStr = [_categorTwo objectAtIndex:indexPath.row];
+            _strTableViewCell = [_arrayCategorTwo objectAtIndex:indexPath.row];
             break;
     }
     
 
-    cell.textLabel.text = _aStr;
+    cell.textLabel.text = _strTableViewCell;
     
     return cell;
 }
-
- 
- 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
